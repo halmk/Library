47c05efc6a9364fddd5bdda482d49b777224e913
halmk <37671081+halmk@users.noreply.github.com>
2018-09-22 18:50:21 +0900

    add main()

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -7,7 +7,7 @@ typedef pair<int, int> P;   // firstは最短距離、secondは頂点の番号
 
 const int MAX_V=1000, MAX_E=1000;
 
-int V;
+int V, E;
 vector<edge> G[MAX_V];
 int d[MAX_V];
 
@@ -30,4 +30,15 @@ void dijkstra(int s){
             }
         }
     }
+}
+int main() {
+	cin >> V >> E;
+	for (int i = 0; i < E; i++) {
+		int s, t, cost;
+		cin >> s >> t >> cost;
+		s--; t--;
+		G[s].push_back(edge{ t, cost });
+		G[t].push_back(edge{ s, cost });
+	}
+	// 処理
 }
