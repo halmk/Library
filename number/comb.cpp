3e93045e2a0d7958c5cfa34ecd0d4d9b1dffdf9c
halmk <elle2206@i.softbank.jp>
2018-12-10 13:23:11 +0900

    fix n add

---
+++ number/comb.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+const int MAX = 510000;
+const int MOD = (int)1e9+7;
+
+ll fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void comb_init(){
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for(int i=2; i<MAX; i++){
+        fac[i] = fac[i-1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i-1] * inv[i] % MOD;
+    }
+}
+
+ll comb(int n, int k){
+    if(n < k) return 0;
+    if(n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n-k] % MOD) % MOD;
+}
+
+int main() {
+    // 前処理
+    comb_init();
+    
+    // 計算例
+    cout << comb(4, 2) << endl;
+}
