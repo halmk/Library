812efdd9205e6915c34912bd83debbce40b700a5
halmk <elle2206@i.softbank.jp>
2019-02-03 13:25:05 +0900

    fix commit

--- graph/prim.cpp
+++ graph/prim.cpp
@@ -1,41 +1,52 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
 #define INF (1 << 29)
 
-const int MAX_V=1000;
+const int MAX_V = 1000;
 
-int cost[MAX_V][MAX_V];
-int mincost[MAX_V];
-bool used[MAX_V];
-int V;
+int cost[MAX_V][MAX_V];	// グラフの隣接行列
+int mincost[MAX_V];	// MSTからMSTでないの頂点へ行く最小コスト
+bool used[MAX_V];	// MSTの属しているかどうか
+int V;	// 頂点数
 
-int prim(){
-    for(int i=0; i<V; i++){
-        mincost[i] = INF;
-        used[i] = false;
-    }
-    mincost[0] = 0;
-    int res = 0;
-    
-    while(true){
-        int v=-1;
-        // Xに属さない頂点のうちXからの辺のコストが最小になる頂点を探す
-        for(int u=0; u<V; u++){
-            if(!used[u] && (v==-1 || mincost[u] < mincost[v])) v=u;
-        }
-        
-        if(v==-1) break;
-        used[v] = true;     // 頂点vをXに追加
-        res += mincost[v];  // 辺のコストを加える
-        
-        for(int u=0; u<V; u++){
-            mincost[u] = min(mincost[u], cost[v][u]);
-        }
-    }
-    return res;
+int prim() {
+	for (int i = 0; i<V; i++) {
+		mincost[i] = INF;
+		used[i] = false;
+	}
+	mincost[0] = 0;
+	int res = 0;	// MSTのコストの総和
+
+	while (true) {
+		int v = -1;
+		// MSTに属さない頂点のうちMSTからの辺のコストが最小になる頂点を探す
+		for (int u = 0; u<V; u++) {
+			if (!used[u] && (v == -1 || mincost[u] < mincost[v])) v = u;
+		}
+
+		if (v == -1) break;
+		used[v] = true;     // 頂点 v をMSTに追加
+		res += mincost[v];  // 頂点 v が確定したので辺のコストを加える
+
+		// 頂点 v に隣接している頂点を調べ、その時点での頂点 u への最小コストを更新する
+		for (int u = 0; u<V; u++) {
+			mincost[u] = min(mincost[u], cost[v][u]);
+		}
+	}
+	return res;
 }
 
 
-int main(){
-    
+int main() {
+	cin >> V;
+
+	// グラフが隣接行列で与えられた場合の入力
+	for (int i = 0; i<V; i++) {
+		for (int j = 0; j<V; j++) {
+			int e; cin >> e;
+			cost[i][j] = (e == -1) ? INF : e;
+		}
+	}
+
+	cout << prim() << endl;
 }
---
+++ graph/prim_rasen.cpp
@@ -0,0 +1,76 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+
+static const int MAX = 100;
+static const int INFTY = (1<<21);
+static const int WHITE = 0;
+static const int GRAY = 1;
+static const int BLACK = 2;
+
+int n, M[MAX][MAX];	// n:頂点数, M[][]:隣接行列
+
+// primのアルゴリズム
+int prim() {
+	int u, minv;
+	// d[n]:d[v]にTに属する頂点とV-Tに属する頂点をつなぐ辺の中で、重みが最小の辺の重みを記録
+	// p[n]:p[v]にMSTにおける頂点vの親を記録する
+	int d[MAX], p[MAX], color[MAX];
+
+	// 初期化
+	for (int i = 0; i < n; i++) {
+		d[i] = INFTY;
+		p[i] = -1;
+		color[i] = WHITE;
+	}
+
+	d[0] = 0;
+
+	while (1) {
+		minv = INFTY;
+		u = -1;
+		// VとV-Tをつなぐ辺のうち最小の辺の重みとなる頂点 u を探す
+		for (int i = 0; i < n; i++) {
+			if (minv > d[i] && color[i] != BLACK) {
+				u = i;
+				minv = d[i];
+			}
+		}
+		if(u == -1) break;
+		// 頂点 u をMSTに追加
+		color[u] = BLACK;
+
+		for (int v = 0; v < n; v++) {
+			if (color[v] != BLACK && M[u][v] != INFTY) {
+				if (d[v] > M[u][v]) {
+					// この時点での頂点 v への最小コストを更新する。またその親を u とする
+					d[v] = M[u][v];
+					p[v] = u;
+					color[v] = GRAY;
+				}
+			}
+		}
+	}
+	// 最小全域木の総和を計算　最適な辺は頂点 i と 頂点 p[i](頂点iの親) を結ぶ辺となる。
+	int sum=0;
+	for (int i = 0; i < n; i++) {
+		if(p[i] != -1) sum += M[i][p[i]];
+	}
+
+	return sum;
+}
+
+int main() {
+	cin >> n;
+
+	// 隣接行列が与えられる
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			int e; cin >> e;
+			M[i][j] = (e == -1) ? INFTY : e;
+		}
+	}
+
+	cout << prim() << endl;
+}
