5e8d13f7304f94f54033764aaf11d05385fda4ba
halmk <elle2206@i.softbank.jp>
2019-02-04 12:14:19 +0900

    fix commit

--- graph/TopologicalSort_dfs.cpp
+++ graph/TopologicalSort_dfs.cpp
@@ -1,43 +1,49 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <list>
+#include "bits/stdc++.h"
 using namespace std;
+
+typedef long long ll;
+
 static const int MAX = 100000;
+static const int INF = (1 << 29);
 
-vector<int> G[MAX];
-list<int> out;
-bool V[MAX];
-int N;
+vector<int> G[MAX];	// 隣接リスト
+list<int> out;	// トポロジカルソートした結果を格納するリスト
+bool V[MAX];	// その頂点が完了したかどうか
+int N;			// 頂点数
+int indeg[MAX];	// その頂点の入次数
 
-void dfs(int u){
-    V[u] = true;
-    for(int i=0; i<G[u].size(); i++){
-        int v = G[u][i];
-        if(!V[v]) dfs(v);
-    }
-    out.push_front(u);
+// 最後に行うものから順にリストに追加していく深さ優先探索
+void dfs(int u) {
+	V[u] = true;
+	for (int i = 0; i < G[u].size(); i++) {
+		int v = G[u][i];
+		// 完了していない頂点を深さ優先探索
+		if(!V[v]) dfs(v);
+	}
+	out.push_front(u);
 }
 
-int main(){
-    int s, t, M;
-    
-    cin >> N >> M;
-    
-    for(int i=0; i<N; i++) V[i] = false;
-    
-    for(int i=0; i<M; i++){
-        cin >> s >> t;
-        G[s].push_back(t);
-    }
-    
-    for(int i=0; i<N; i++){
-        if(!V[i]) dfs(i);
-    }
-    
-    for(list<int>::iterator it = out.begin(); it != out.end(); it++){
-        cout << *it << endl;
-    }
-    
-    return 0;
+int main() {
+	int s, t, M;
+	cin >> N >> M;
+
+	for(int i=0; i<N; i++) V[i] = false;
+
+	// 閉路なし有向グラフ
+	for (int i = 0; i < M; i++) {
+		cin >> s >> t;
+		G[s].push_back(t);
+	}
+
+	for (int i = 0; i < N; i++) {
+		// 完了していない頂点を深さ優先探索
+		if (!V[i]) {
+			dfs(i);
+		}
+	}
+	
+	for (list<int>::iterator it = out.begin(); it != out.end(); it++) {
+		cout << *it << endl;
+	}
+
 }
